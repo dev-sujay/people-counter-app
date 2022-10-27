51fab3f8a3233d4e440546e9be16ca8b787cba50
Sujay Paul <sujaypaul531@gmail.com>
2022-10-28 02:29:12 +0530

    Added files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let counter = 0
+
+
+
+function increment() {
+    counter = counter + 1
+    document.getElementById("count-el").innerText = counter
+}
+
+function refresh()  {
+    counter = 0
+    document.getElementById("count-el").innerText = 0
+
+}
+
+function save() {   
+    console.log(counter)
+}
+
+
